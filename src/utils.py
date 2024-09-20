b54c2d646c54d0665743410e831082f0507217ca
Vica Worth <11998960+VicaWorth@users.noreply.github.com>
2024-09-20 09:31:57 -0700

    Added utils

---
+++ src/utils.py
@@ -0,0 +1,14 @@
+"""
+The Utils class holds common functions that can be used in:
+- Encoding
+- Preprocessing
+- Visualizations
+"""
+
+class Utils():
+    def download_w_and_l_folders_collab():
+        print("Goes here")
+    
+    # Common Categorical Encoding Functions
+    
+    # Common Timeseries Encoding Functions
